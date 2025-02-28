d5cdc6be4a13ab65b15b256473cdd942ea6bfe70
Runuk <dpg383@gmail.com>
2025-02-27 19:02:47 -0700

    Fix: Add export to ErrorBoundary component

--- src/components/shared/ErrorBoundary.tsx
+++ src/components/shared/ErrorBoundary.tsx
@@ -11,7 +11,7 @@ interface State {
   errorInfo: ErrorInfo | null;
 }
 
-class ErrorBoundary extends Component<Props, State> {
+export class ErrorBoundary extends Component<Props, State> {
   public state: State = {
     hasError: false,
     error: null,
@@ -78,6 +78,4 @@ class ErrorBoundary extends Component<Props, State> {
 
     return this.props.children;
   }
-}
-
-export default ErrorBoundary; 
+} 
